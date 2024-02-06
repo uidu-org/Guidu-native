0fe79073a3e6af53d4b3fa1038e62f46ef0b8364
Andrea Vanini <andrea.vanini@uidu.org>
2024-02-06 14:24:04 +0100

    Manypkg fix on select-ui + editorconfig

--- apps/next/src/app/docs/page.tsx
+++ apps/next/src/app/docs/page.tsx
@@ -1,16 +1,11 @@
-import Link from "next/link";
+import Link from 'next/link'
 
 export default function IndexDocsPage() {
-    return (
-        <div className="min-h-dvh">
-
-            <div>IndexDocsPage</div>
-            <Link href="/docs/core/button">
-                Go to Button
-            </Link>
-            <Link href="/docs/forms">
-                Go to forms
-            </Link>
-        </div>
-    )
+  return (
+    <div className="min-h-dvh">
+      <div>IndexDocsPage</div>
+      <Link href="/docs/core/button">Go to Button</Link>
+      <Link href="/docs/forms">Go to forms</Link>
+    </div>
+  )
 }

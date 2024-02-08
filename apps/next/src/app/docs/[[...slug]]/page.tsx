4d1956038c30c295ca1378feebb3f2ded5c63d86
Flavio Scimeca <flavioscimeca3@gmail.com>
2024-02-08 23:24:54 +0100

    add DocsToc (WIP => mdx required)

--- apps/next/src/app/docs/[[...slug]]/page.tsx
+++ apps/next/src/app/docs/[[...slug]]/page.tsx
@@ -1,4 +1,6 @@
+import { DocsToc } from "@/components/layout/DocsToc";
 import { componentList } from "@/config/docsComponent";
+import { getHeadings } from "@/lib/getHeadings";
 
 interface ComponentList {
     [key: string]: React.FC;
@@ -19,15 +21,25 @@ export default function DocsPages({ params }: DocPageProps) {
     if (!componentList.hasOwnProperty(slug)) return null
     const Component = componentList[slug]
 
+    const headings = getHeadings(
+        //docs stringify per leggere e creare i #inner anchor link
+    );
+
     return (
+        <>
+            <div className="col-span-12 lg:col-span-10 xl:col-span-8 lg:px-16 mt-10">
+                <div className="w-full prose prose-neutral">
 
-        <div className="col-span-12 lg:col-span-10 xl:col-span-8 lg:px-16 mt-10">
-            <div className="w-full prose prose-neutral">
+                    <Component />
 
-                <Component />
-
+                </div>
             </div>
-        </div>
+            {headings && headings.length > 0 && (
+                <div className="hidden z-10 xl:flex xl:col-span-2 mt-8 pl-4">
+                    <DocsToc headings={headings} />
+                </div>
+            )}
+        </>
 
     )
 }

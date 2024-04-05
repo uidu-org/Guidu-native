0ebe0fdd5660abfbc086409cb44f93719b7cfeb6
Andrea Vanini <andrea.vanini@uidu.org>
2024-04-05 16:57:54 +0200

    Bump deps

--- apps/expo/app/docs/chat.tsx
+++ apps/expo/app/docs/chat.tsx
@@ -4,64 +4,6 @@ import { Stack } from 'expo-router';
 import { useCallback, useMemo, useRef, useState } from 'react';
 
 export default function DocsChatPage() {
-
-    const listRef = useRef(null);
-
-    const fakeUsers: GMessage[] = useMemo(() => (
-        new Array(70).fill(null).map(() => ({
-            id: faker.number.int({ max: 99999 }),
-            text: faker.lorem.sentence(), // Consider using sentence() for chat messages
-            itsMe: faker.datatype.boolean(),
-            createdAt: faker.date.anytime(),
-            user: {
-                id: faker.number.int({ max: 99999 }),
-                name: faker.person.fullName(),
-                avatar: { uri: faker.image.avatar() },
-            },
-            ...(faker.datatype.boolean() && {
-                media: [{
-                    uri: faker.image.url(),
-                    type: 0
-                }]
-            })
-        }))
-    ), []);
-
-    const [messages, setMessages] = useState<GMessage[]>
-        (fakeUsers)
-
-    const mentions = useMemo(() => {
-        return messages.map((m) => m.user)
-    }, [messages])
-
-    const _onPressSend = useCallback(
-        (text: string, repliedTo?: GMessage) => {
-            console.log("testo parent", text);
-
-            //@ts-ignore
-            listRef.current.appendMessage({
-                id: messages.length + 1,
-                text: text,
-                itsMe: true,
-                createdAt: new Date(),
-                user: currentUser,
-                ...(repliedTo && {
-                    repliedTo: repliedTo
-                })
-            });
-            console.log("hey");
-        },
-        // ...(faker.datatype.boolean() && {
-        //     media: [{
-        //         uri: faker.image.url(),
-        //         type: 0
-        //     }]
-        // })
-      })),
-    []
-  );
-
-  const message = useRef<string>('');
   const listRef = useRef(null);
 
   const fakeUsers: GMessage[] = useMemo(
@@ -93,6 +35,8 @@ export default function DocsChatPage() {
   const mentions = useMemo(() => {
     return messages.map((m) => m.user);
   }, [messages]);
+
+  const message = useRef<string>('');
 
   const _onPressSend = useCallback((text: string, repliedTo?: GMessage) => {
     console.log('testo parent', text);
@@ -131,7 +75,7 @@ export default function DocsChatPage() {
 }
 
 const currentUser = {
-    id: 1,
-    name: "John Doe",
-    avatar: { uri: "https://i.pravatar.cc/300" },
-}
+  id: 1,
+  name: 'John Doe',
+  avatar: { uri: 'https://i.pravatar.cc/300' },
+};

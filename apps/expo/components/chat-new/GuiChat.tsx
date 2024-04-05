ab94b03a04e9989088ac61cd10b65ba62ccb8718
Andrea Vanini <andrea.vanini@uidu.org>
2024-04-05 18:43:52 +0200

    Try fix GuiChat flex

--- apps/expo/components/chat-new/GuiChat.tsx
+++ apps/expo/components/chat-new/GuiChat.tsx
@@ -1,15 +1,14 @@
-import { GuiText } from '@uidu/native'
-import React, { ForwardedRef, useEffect, useRef } from 'react'
-import { Keyboard, KeyboardAvoidingView, Platform, SafeAreaView } from 'react-native'
-import ChatFooter from './ChatFooter'
-import { ChatList } from './ChatList'
-import { ChatContextProvider } from './context/WrapperContext'
-import { GChatty, ListRef } from './types'
-
+import { GuiText } from '@uidu/native';
+import React, { ForwardedRef, useEffect, useRef } from 'react';
+import { Keyboard, KeyboardAvoidingView, Platform, View } from 'react-native';
+import ChatFooter from './ChatFooter';
+import { ChatList } from './ChatList';
+import { ChatContextProvider } from './context/WrapperContext';
+import { GChatty, ListRef } from './types';
 
 export const GuiChat = React.forwardRef(
   (props: GChatty, ref: ForwardedRef<ListRef>) => {
-    const { messages, currentUser, mentions } = props
+    const { messages, currentUser, mentions } = props;
 
     const listRef = useRef<ListRef>();
 
@@ -31,31 +30,42 @@ export const GuiChat = React.forwardRef(
     }, [ref]);
 
     return (
-      <SafeAreaView>
-        <ChatContextProvider>
-          <KeyboardAvoidingView
-            behavior={Platform.select({
-              android: 'position',
-              ios: 'position',
-            })}
-            keyboardVerticalOffset={Platform.select({
-              android: -30,
-            })}
-          >
-            {
-              messages.length < 1 ? (
-                <GuiText>Empty chat</GuiText>
-              ) : (
-                <ChatList
-                  data={messages}
-                  //@ts-ignore
-                  ref={ref ?? listRef}
-                />
-              )
-            }
-            <ChatFooter onChangeText={props?.onChangeText} value={props.value} mentions={mentions} onPressSend={props.onPressSend} />
-          </KeyboardAvoidingView>
-        </ChatContextProvider>
-      </SafeAreaView>
-    )
-  })
+      <ChatContextProvider>
+        <KeyboardAvoidingView
+          behavior={Platform.select({
+            android: undefined,
+            ios: 'padding',
+          })}
+          // keyboardVerticalOffset={Platform.select({
+          //   android: -30,
+          //   ios: -30,
+          // })}
+          style={{
+            flex: 1,
+            // backgroundColor: 'purple',
+          }}
+        >
+          <View style={{ flex: 1, flexGrow: 1, height: '100%' }}>
+            {messages.length < 1 ? (
+              <GuiText>Empty chat</GuiText>
+            ) : (
+              <ChatList
+                data={messages}
+                //@ts-ignore
+                ref={ref ?? listRef}
+              />
+            )}
+          </View>
+          <View style={{ maxHeight: 100, flexShrink: 0 }}>
+            <ChatFooter
+              onChangeText={props?.onChangeText}
+              value={props.value}
+              mentions={mentions}
+              onPressSend={props.onPressSend}
+            />
+          </View>
+        </KeyboardAvoidingView>
+      </ChatContextProvider>
+    );
+  }
+);

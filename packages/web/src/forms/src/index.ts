22a9e2f90390195f7e33eaf91691990ed3b85e5e
Flavio Scimeca <flavioscimeca3@gmail.com>
2024-04-29 00:30:36 +0200

    publish all forms component and released @uidu/forms-ui

--- packages/web/src/forms/src/index.ts
+++ packages/web/src/forms/src/index.ts
@@ -1,29 +1,14 @@
-export {
-  Calendar,
-  DatePicker,
-  RHFCalendar,
-  type CalendarProps,
-  type DatePickerProps,
-} from '@uidu/calendar-ui';
+export * from '@uidu/calendar-ui';
 export * from '@uidu/checkbox-ui';
 // export { Avatar, AvatarFallback, AvatarImage } from '@uidu/color-picker-ui'
-export {
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-  Form as UiForm,
-  useFormField,
-} from '@uidu/form-ui';
-export { Input, RHFInput, type InputProps } from '@uidu/input-ui';
-export { Multiselect } from '@uidu/multiselect-ui';
-export { PinCode, RHFPinCode, type PinCodeProps } from '@uidu/pin-code-ui';
+export * from '@uidu/form-ui';
+export * from '@uidu/input-ui';
+export * from '@uidu/multiselect-ui';
+// export * from '@uidu/pin-code-ui';
 export * from '@uidu/radio-group-ui';
 export * from '@uidu/select-ui';
 export * from '@uidu/slider-ui';
-export { Stepper, type StepperProps } from '@uidu/stepper-ui';
+export * from '@uidu/stepper-ui';
 export * from '@uidu/switch-ui';
-export { RHFTextarea, Textarea, type TextareaProps } from '@uidu/textarea-ui';
+export * from '@uidu/textarea-ui';
 export * from '@uidu/toggle-ui';

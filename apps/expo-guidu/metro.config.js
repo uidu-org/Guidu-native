7ecc4b07e148c0b67a3e38b1fb6b86ae3bde196a
Flavio Scimeca <flavioscimeca3@gmail.com>
2024-04-28 22:15:29 +0200

    change metro.config and expo-router/entry

--- apps/expo-guidu/metro.config.js
+++ apps/expo-guidu/metro.config.js
@@ -1,10 +1,23 @@
 //! EXPO PROBLEM WITH MONOREPO => https://github.com/expo/expo/issues/19870
 
-// Learn more https://docs.expo.io/guides/customizing-metro
+//
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+// Find the project and workspace directories
+const projectRoot = __dirname;
+// This can be replaced with `find-yarn-workspace-root`
+const monorepoRoot = path.resolve(projectRoot, '../..');
+
+const config = getDefaultConfig(projectRoot);
 config.resolver.disableHierarchicalLookup = true;
+
+// 1. Watch all files within the monorepo
+config.watchFolders = [monorepoRoot];
+// 2. Let Metro know where to resolve packages and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(monorepoRoot, 'node_modules'),
+];
 
 module.exports = config;

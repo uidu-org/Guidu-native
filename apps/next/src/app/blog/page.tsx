33fbd472d8b69ee5fccd8f8ca6a50e9d3c7098c6
Giacomo Giovannetti <giacomo.giovannetti@alboran.local>
2024-05-03 15:51:36 +0200

    refactoring @uidu/badge-ui

--- apps/next/src/app/blog/page.tsx
+++ apps/next/src/app/blog/page.tsx
@@ -1,6 +1,6 @@
 import BreadcrumbDemo from "@/docs/demos/breadcrumb-demo";
 import ToastDemo from "@/docs/demos/toast-demo";
-import { Button, ButtonGroup } from "@uidu/core-ui";
+import { Badge, Button } from "@uidu/core-ui";
 import Link from "next/link";
 
 
@@ -11,13 +11,21 @@ export default function Page() {
       <Link className="underline" href={"/"}>Go Back</Link>
       <BreadcrumbDemo />
       <ToastDemo />
-      <ButtonGroup>
-        
-        <Button isLoading className="text-black" >icona</Button>
+      <div className="w-20 h-auto m-5 ">
+
+      <Button className="text-black" >icona</Button>
         <Button variant={"danger"} className="text-black" >danger</Button>
         <Button  className="text-black" >icona</Button>
+      </div>
        
-      </ButtonGroup>
+      <div className="flex flex-col justify-start">
+     <div> <span>Default</span><Badge variant={'default'} max={10} testId={"prova"} >{6}</Badge></div>
+      <div><span>Added</span><Badge variant={'added'} max={30}>{23}</Badge></div>
+      <div><span>Important</span><Badge variant={'important'} max={50}>{55}</Badge></div>
+      <div><span>Primary</span><Badge variant={'primary'} max={70}>{2}</Badge></div>
+      <div><span>PrimaryInverted</span><Badge variant={'primaryInverted'} max={99}>{100}</Badge></div>
+      <div><span>Removed</span><Badge variant={'removed'} max={100}>{10000}</Badge></div>
+      </div>
     </div>
   )
 }
--- packages/web/components/core/badge/src/badge.tsx
+++ packages/web/components/core/badge/src/badge.tsx
@@ -1,36 +1,61 @@
-import { cva, type VariantProps } from "class-variance-authority"
-import * as React from "react"
+import { cva, type VariantProps } from 'class-variance-authority';
+import * as React from 'react';
+import { FormatValue } from './FormatValue';
 
-import { cn } from "@uidu/lib"
+import { cn } from '@uidu/lib';
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-full border border-stone-200 px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-stone-950 focus:ring-offset-2 dark:border-stone-800 dark:focus:ring-stone-300",
+  'inline-flex items-center rounded-full border border-stone-200 px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-stone-950 focus:ring-offset-2 dark:border-stone-800 dark:focus:ring-stone-300',
   {
     variants: {
       variant: {
+        // outline: 'text-stone-950 dark:text-stone-50',
+
         default:
-          "border-transparent bg-stone-900 text-stone-50 hover:bg-stone-900/80 dark:bg-stone-50 dark:text-stone-900 dark:hover:bg-stone-50/80",
-        secondary:
-          "border-transparent bg-stone-100 text-stone-900 hover:bg-stone-100/80 dark:bg-stone-800 dark:text-stone-50 dark:hover:bg-stone-800/80",
-        destructive:
-          "border-transparent bg-red-500 text-stone-50 hover:bg-red-500/80 dark:bg-red-900 dark:text-stone-50 dark:hover:bg-red-900/80",
-        outline: "text-stone-950 dark:text-stone-50",
+          'border-transparent bg-uiduThemes-neutral-40 text-uiduThemes-neutral-800 hover:bg-uiduThemes-neutral-40/80 dark:bg-uiduThemes-darkNeutral-70 dark:text-uiduThemes-darkNeutral-900 dark:hover:bg-uiduThemes-darkNeutral-70/80',
+        added:
+          'border-transparent bg-uiduThemes-green-50 text-uiduThemes-green-500 hover:bg-uiduThemes-green-50/80',
+        important:
+          'border-transparent bg-uiduThemes-red-400 text-uiduThemes-neutral-0 hover:bg-uiduThemes-red-400/80',
+        primary:
+          'border-transparent bg-uiduThemes-blue-400 text-uiduThemes-neutral-0 hover:bg-uiduThemes-blue-400/80 dark:bg-uiduThemes-blue-100 dark:text-uiduThemes-darkNeutral-0 dark:hover:bg-uiduThemes-blue-100/80',
+        primaryInverted:
+          'border-transparent bg-uiduThemes-neutral-0 text-uiduThemes-blue-500 hover:bg-uiduThemes-neutral-0/80 dark:bg-uiduThemes-darkNeutral-400 dark:text-uiduThemes-darkNeutral-0 dark:hover:bg-uiduThemes-darkNeutral-400/80',
+        removed:
+          'border-transparent bg-uiduThemes-red-50 text-uiduThemes-red-500 hover:bg-uiduThemes-red-50/80',
       },
     },
     defaultVariants: {
-      variant: "default",
+      variant: 'default',
     },
   }
-)
+);
 
 export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
-  VariantProps<typeof badgeVariants> { }
-
-function Badge({ className, variant, ...props }: BadgeProps) {
-  return (
-    <div className={cn(badgeVariants({ variant }), className)} {...props} />
-  )
+    VariantProps<typeof badgeVariants> {
+  children?: number | string;
+  max?: number;
+  testId?: string;
 }
 
-export { Badge, badgeVariants }
+function Badge({
+  className,
+  variant,
+  children = 0,
+  max = 99,
+  testId,
+  ...props
+}: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props}>
+      {typeof children === 'string' ? (
+        children
+      ) : (
+        <FormatValue max={max}>{children}</FormatValue>
+      )}
+    </div>
+  );
+}
+
+export { Badge, badgeVariants };

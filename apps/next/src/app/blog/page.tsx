d5d6a17125c582730470b9f0e0439bbdffaadcd7
Giacomo Giovannetti <giacomo.giovannetti@alboran.local>
2024-05-06 10:59:31 +0200

    reorganized components demos between two folders (core -forms) and relative imports in mdx files

--- apps/next/src/app/blog/page.tsx
+++ apps/next/src/app/blog/page.tsx
@@ -4,11 +4,13 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className='grid place-content-center h-screen'>
+    <div className="grid h-screen place-content-center">
       <p>Page Blog</p>
-      <Link className="underline" href={"/"}>Go Back</Link>
+      <Link className="underline" href={"/"}>
+        Go Back
+      </Link>
       <BreadcrumbDemo />
       <ToastDemo />
     </div>
-  )
+  );
 }

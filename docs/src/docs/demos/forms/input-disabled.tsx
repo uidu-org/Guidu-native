7e66ed92a0732f731618e437fcb5e7b1fd7af501
Giacomo Giovannetti <giacomo.giovannetti@alboran.local>
2024-05-24 12:55:17 +0200

    added missing demos for documentation, cannot build packages because tsup is not found

---
+++ docs/src/docs/demos/forms/input-disabled.tsx
@@ -0,0 +1,5 @@
+import { Input } from "@uidu/forms-ui";
+
+export default function InputDisabled() {
+  return <Input disabled type="email" placeholder="Email" />;
+}
---
+++ docs/src/docs/demos/forms/input-with-label.tsx
@@ -0,0 +1,11 @@
+import { Label } from "@uidu/core-ui";
+import { Input } from "@uidu/forms-ui";
+
+export default function InputWithLabel() {
+  return (
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Label htmlFor="email">Email</Label>
+      <Input type="email" id="email" placeholder="Email" />
+    </div>
+  );
+}

b870ad17753a13c8c50aca867bd60942635d1622
Andrea Vanini <andrea.vanini@uidu.org>
2024-05-28 10:42:29 +0200

    Fix react-hook-form package version

--- packages/components/forms/toggle/src/RHFToggle.tsx
+++ packages/components/forms/toggle/src/RHFToggle.tsx
@@ -1,30 +1,42 @@
-import { Control, Controller, FieldValues, RegisterOptions } from "react-hook-form";
-import { Toggle } from "./toggle";
+import {
+  Control,
+  Controller,
+  FieldValues,
+  RegisterOptions,
+} from 'react-hook-form';
+import { Toggle } from './toggle';
 
-
-export function RHFToggle({ name, control, rules, ...rest }: {
-    name: string
-    control: Control;
-    rules: Omit<
-        RegisterOptions<FieldValues, string>,
-        'valueAsNumber' | 'valueAsDate' | 'setValueAs' | 'disabled'
-    >
+export function RHFToggle({
+  name,
+  control,
+  rules,
+  ...rest
+}: {
+  name: string;
+  control: Control;
+  rules: Omit<
+    RegisterOptions<FieldValues, string>,
+    'valueAsNumber' | 'valueAsDate' | 'setValueAs' | 'disabled'
+  >;
 }) {
-    return (
-        <Controller
-            name={name}
-            control={control}
-            rules={rules}
-            render={({ field: { name, onChange, ref, value = "", disabled, onBlur } }) => (
-                <Toggle
-                    id={name}
-                    onPressedChange={onChange}
-                    ref={ref}
-                    value={value}
-                    disabled={disabled}
-                    onBlur={onBlur}
-                    {...rest}
-                />
-            )} />
-    )
+  return (
+    <Controller
+      name={name}
+      control={control}
+      rules={rules}
+      render={({
+        field: { name, onChange, ref, value = '', disabled, onBlur },
+      }) => (
+        <Toggle
+          id={name}
+          onPressedChange={onChange}
+          ref={ref}
+          value={value}
+          disabled={disabled}
+          onBlur={onBlur}
+          {...rest}
+        />
+      )}
+    />
+  );
 }
